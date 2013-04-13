220446d0badbe23433cc951844e2300e4cb0a919
Jason Goodell <jgoodell77@gmail.com>
2013-04-12 20:37:12 -0400

    Added a basic view with a hello world, a urls.py to the app and configured git.

---
+++ code_challenge/tic_tac_toe/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+from tic_tac_toe import views
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'code_challenge.views.home', name='home'),
+    # url(r'^code_challenge/', include('code_challenge.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+                       url(r'',views.index, name='index')
+)
